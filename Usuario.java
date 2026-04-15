e80cb4fead2d94f9b3961fc2ea1766e64e8a4de3
LorenzoIrigoite <lorenzo.maciel25@edu.pucrs.br>
2026-04-15 12:50:55 -0300

    Claass Usuario

--- Usuario.java
+++ Usuario.java
@@ -1,3 +1,25 @@
-public class Usuario {
+public abstract class Usuario {
+    private final String nome; //deixei final pelo o que entendi nao vamos alterar cadastro.
+    private final String iniciais;
+    private int id= 1;
+   
+    public Usuario(String nome, String iniciais) {
+        this.nome = nome;
+        this.iniciais = iniciais;
+        this.id = id++; //id autoincrementado
+    }
+
+    //deixei uns gets aqui caso fosse necessario, qualquer coisa apaga
+    public String getNome() {
+        return nome;
+    }
+
+    public String getIniciais() {
+        return iniciais;
+    }
+
+    public int getId() {
+        return id;
+    }
 
 }
