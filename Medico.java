b805d066e7d81f8584c09dc12f7d72251d8d87da
André Menezes <1992menezes@gmail.com>
2026-04-15 15:11:43 -0300

    Criar classe Medico com métodos vazios

---
+++ Medico.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Medico extends Usuario {
+    public boolean incluirAutorizacaoExame() {
+
+    }
+
+    public ArrayList<AutorizacaoExame> listarAutorizacaoExamesPorPaciente(Paciente paciente) {
+
+    }
+
+    public ArrayList<AutorizacaoExame> listarAutorizacaoExamesPorTipo(TipoExame tipoExame) {
+
+    }
+}
