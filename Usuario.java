6e4afce2cdc858225fcb9338d8c37c09538a2ad0
LorenzoIrigoite <lorenzo.maciel25@edu.pucrs.br>
2026-04-15 14:00:39 -0300

    Metodo Iniciais auto + fix sintaxe mais clara

--- Usuario.java
+++ Usuario.java
@@ -4,23 +4,35 @@ public abstract class Usuario {
     private final int id;
     private static int incrementoId= 1;
    
-    public Usuario(String nome, String iniciais) {
+    public Usuario(String nome) {
         this.nome = nome;
-        this.iniciais = iniciais;
+        this.iniciais = geradorIniciais(nome);
         this.id = incrementoId++; //id autoincrementado
+    }
+
+    
+    private String geradorIniciais(String nome) {
+        String[] partesNome = nome.split(" ");
+        StringBuilder inicial = new StringBuilder();
+        for (String parte : partesNome) {
+            if (!parte.isEmpty()) {
+                inicial.append(parte.charAt(0)); //pega a primeira letra de cada parte do nome
+            }
+        }
+        return inicial.toString().toUpperCase(); //retorna as iniciais em maiúsculo
     }
 
     //deixei uns gets aqui caso fosse necessario, qualquer coisa apaga
     public String getNome() {
-        return nome;
+        return this.nome;
     }
 
     public String getIniciais() {
-        return iniciais;
+        return this.iniciais;
     }
 
     public int getId() {
-        return id;
+        return this.id;
     }
 
 }
